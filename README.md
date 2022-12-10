a68ec6273d09db4f279ba87b8ee9598511260f38
MrTizu <99123684+MrTizu@users.noreply.github.com>
2022-12-10 09:22:20 +0200

    Blog first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Minimal Neatio Blog Site
+
+Written in TypeScript
+
+Built with Vue3 and Markdown
+
+Clone repo
+
+```
+https://github.com/neatio-network/neat-blog.git
+```
+
+Dependencies installation:
+
+```
+npm install
+```
+
+For development run:
+
+```
+npm run dev
+```
+
+For production run:
+
+```
+npm run dist
+```
---
+++ src/components/BlogHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="text-center my-5">
+
+      <div class="display-3">
+        Neatio Blog
+      </div>
+      <img
+        alt="Vue logo"
+        src="../assets/logo.png"
+      >
+  </div>
+</template>
+<style scoped>
+.display-3 {
+  color: #000;
+  font-family: Pirulen, Arial, Helvetica, sans-serif;
+  text-transform: uppercase;
+}
+</style>
